51d3f7fa3c39988f7aea671dedfa3e42e0243d94
chaijewon <vcandjava@nate.com>
2025-12-24 11:31:46 +0900

    1111

--- src/main/java/com/sist/web/mapper/SeoulMapper.java
+++ src/main/java/com/sist/web/mapper/SeoulMapper.java
@@ -2,6 +2,7 @@ package com.sist.web.mapper;
 import java.util.*;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import com.sist.web.vo.*;
@@ -32,4 +33,20 @@ public interface SeoulMapper {
     */
 	public int seoulTotalPage(Map map);
 	// => 인터페이스는 서로 다른 클래스 연결 (리모콘) => 모든 변수/모든 메소드는 public
+	
+	/*
+	 *   <select id="seoulDetailData" resultType="com.sist.web.vo.SeoulVO"
+		   parameterType="hashmap"
+		  >
+		    SELECT no,title,poster,address,hit
+		    FROM ${table_name}
+		    WHERE no=#{no}
+		  </select>
+	 */
+	@Update("UPDATE ${table_name} SET "
+		   +"hit=hit+1 "
+		   +"WHERE no=#{no}")
+	public void seoulHitIncrement(Map map);
+	
+	public SeoulVO seoulDetailData(Map map);
 }
